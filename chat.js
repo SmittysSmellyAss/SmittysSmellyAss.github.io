6306705bdc3208553e354ea2f64614a26b7269cc
weberat0r <135550910+weberat0r@users.noreply.github.com>
2023-06-12 15:19:37 -0700

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,34 @@
+// Connect to WebSocket server
+const socket = new WebSocket('ws://your-server.com');
+
+// Get chat form, input and message display elements
+const chatForm = document.getElementById('message-form');
+const chatInput = document.getElementById('message-input');
+const chatMessages = document.getElementById('chat-messages');
+
+// Listen for a message from server
+socket.onmessage = function(event) {
+    displayMessage(event.data);
+};
+
+// Display a message in chat
+function displayMessage(message) {
+    const messageElement = document.createElement('p');
+    messageElement.textContent = message;
+    chatMessages.appendChild(messageElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight; // Auto scroll to bottom
+}
+
+// Send a message to server
+function sendMessage(message) {
+    socket.send(message);
+}
+
+// Handle form submission
+chatForm.onsubmit = function(event) {
+    event.preventDefault();
+
+    // Send message and clear input
+    sendMessage(chatInput.value);
+    chatInput.value = '';
+}
