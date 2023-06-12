5361881f2aaf3035e538c947d6bf5283a389c18a
weberat0r <135550910+weberat0r@users.noreply.github.com>
2023-06-12 16:10:09 -0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -13,32 +13,44 @@ socket.onmessage = function(event) {
     console.log('Received message from server:', event.data);
     
     const chatLog = document.querySelector('#chat-messages');
-    const newMessage = document.createElement('div');
-    newMessage.textContent = event.data;
-    chatLog.appendChild(newMessage);
+    console.log('Chat log:', chatLog);  // debug line
+    
+    if (chatLog) {
+        const newMessage = document.createElement('div');
+        newMessage.textContent = event.data;
+        chatLog.appendChild(newMessage);
+    }
 };
 
 // Add event listener for chat form submission
-document.querySelector('#message-form').addEventListener('submit', event => {
-    // Prevent the form from refreshing the page
-    event.preventDefault();
+const form = document.querySelector('#message-form');
+console.log('Form:', form);  // debug line
 
-    // Get the message from the text input
-    const input = document.querySelector('#message-input');
-    const message = input.value;
-    input.value = '';
+if (form) {
+    form.addEventListener('submit', event => {
+        // Prevent the form from refreshing the page
+        event.preventDefault();
 
-    // Check if WebSocket is connected before sending message
-    if (socket.readyState === WebSocket.OPEN) {
-        // Send the message to the server
-        socket.send(message);
+        // Get the message from the text input
+        const input = document.querySelector('#message-input');
+        const message = input.value;
+        input.value = '';
 
-        // Append the message to the chat log
-        const chatLog = document.querySelector('#chat-messages');
-        const newMessage = document.createElement('div');
-        const newMessage.textContent = `You: ${message}`;
-        chatLog.appendChild(newMessage);
-    } else {
-        console.error('Cannot send message, WebSocket is not open');
-    }
-});
+        // Check if WebSocket is connected before sending message
+        if (socket.readyState === WebSocket.OPEN) {
+            // Send the message to the server
+            socket.send(message);
+
+            // Append the message to the chat log
+            const chatLog = document.querySelector('#chat-messages');
+            
+            if (chatLog) {
+                const newMessage = document.createElement('div');
+                newMessage.textContent = `You: ${message}`;
+                chatLog.appendChild(newMessage);
+            }
+        } else {
+            console.error('Cannot send message, WebSocket is not open');
+        }
+    });
+}
