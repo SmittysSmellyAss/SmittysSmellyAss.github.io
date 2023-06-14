29b5fa892dcea1c38b39279f7b7e4475c99915e0
weberat0r <135550910+weberat0r@users.noreply.github.com>
2023-06-13 19:47:26 -0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  // Connect to WebSocket server
-  const socket = new WebSocket('wss://rune-thirsty-zipper.glitch.me'); // replace with your WebSocket server URL
+  const WEB_SOCKET_URL = 'wss://rune-thirsty-zipper.glitch.me';
+  const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1117953159072055418/uDR4uH3mtx-ONQih1mVjkxEMKYsF4IBGhpqKsU3lUE64GyD_RdSyFe5TUpk9IaTDhtld';
+
+  const socket = new WebSocket(WEB_SOCKET_URL);
 
   socket.onopen = function () {
     console.log('WebSocket connection opened');
@@ -21,62 +23,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
       newMessage.textContent = message.content;
       chatLog.appendChild(newMessage);
 
-      // Scroll to the bottom of the chat log
       chatLog.scrollTop = chatLog.scrollHeight;
     }
   };
 
-  // Add event listener for chat form submission
   const form = document.querySelector('#message-form');
-
   if (form) {
     form.addEventListener('submit', (event) => {
-      // Prevent the form from refreshing the page
       event.preventDefault();
+      const aliasInput = document.querySelector('#alias-input');
+      const messageInput = document.querySelector('#message-input');
+      const alias = aliasInput.value ? aliasInput.value : 'anon';
+      const message = messageInput.value;
 
-      // Get the message from the text input
-      const input = document.querySelector('#message-input');
-      const message = input.value;
-      input.value = '';
+      if (message) {
+        const timestamp = new Date().toLocaleString();
+        const formattedMessage = `(${timestamp}) ${alias}: ${message}`;
+        socket.send(formattedMessage);
+        messageInput.value = '';
 
-      // Check if WebSocket is connected before sending the message
-      if (socket.readyState === WebSocket.OPEN) {
-        // Send the message to the server
-        socket.send(message);
-      } else {
-        console.error('Cannot send message, WebSocket is not open');
+        const webhookUrl = DISCORD_WEBHOOK_URL;
+        const webhook = new Discord.WebhookClient({ url: webhookUrl });
+        webhook.send(`${alias}: ${message}`).catch(console.error);
       }
-
-      // Send the message to the Discord channel using the webhook
-      const timestamp = new Date().toLocaleString();
-      const formattedMessage = `(Timestamp here) Posch: ${message}`;
-
-      const webhookData = {
-        content: formattedMessage,
-      };
-
-      const webhookUrl = 'https://discord.com/api/webhooks/1117953159072055418/uDR4uH3mtx-ONQih1mVjkxEMKYsF4IBGhpqKsU3lUE64GyD_RdSyFe5TUpk9IaTDhtld'; // Replace with your actual Discord webhook URL
-
-      fetch(webhookUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(webhookData),
-      })
-        .then(function (response) {
-          console.log('Webhook sent successfully!');
-        })
-        .catch(function (error) {
-          console.error('Error sending webhook:', error);
-        });
-
-      // Clear the input field
-      input.value = '';
-
-      // Scroll to the bottom of the chat log
-      const chatLog = document.querySelector('#chat-messages');
-      chatLog.scrollTop = chatLog.scrollHeight;
     });
   }
 });
