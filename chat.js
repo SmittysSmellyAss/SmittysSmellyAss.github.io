3c9230562d53f7b651cf9a054ca1bbbab00bf091
weberat0r <135550910+weberat0r@users.noreply.github.com>
2023-06-13 19:55:03 -0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const WEB_SOCKET_URL = 'wss://rune-thirsty-zipper.glitch.me';
   const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1117953159072055418/uDR4uH3mtx-ONQih1mVjkxEMKYsF4IBGhpqKsU3lUE64GyD_RdSyFe5TUpk9IaTDhtld';
-
   const socket = new WebSocket(WEB_SOCKET_URL);
 
   socket.onopen = function () {
@@ -13,8 +12,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
   };
 
   socket.onmessage = function (event) {
-    console.log('Received message from server:', event.data);
-
     const chatLog = document.querySelector('#chat-messages');
 
     if (chatLog) {
@@ -22,12 +19,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
       const newMessage = document.createElement('div');
       newMessage.textContent = message.content;
       chatLog.appendChild(newMessage);
-
       chatLog.scrollTop = chatLog.scrollHeight;
     }
   };
 
   const form = document.querySelector('#message-form');
+
   if (form) {
     form.addEventListener('submit', (event) => {
       event.preventDefault();
@@ -39,12 +36,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
       if (message) {
         const timestamp = new Date().toLocaleString();
         const formattedMessage = `(${timestamp}) ${alias}: ${message}`;
-        socket.send(formattedMessage);
+        socket.send(JSON.stringify({ author: alias, content: message }));
+
         messageInput.value = '';
 
         const webhookUrl = DISCORD_WEBHOOK_URL;
         const webhook = new Discord.WebhookClient({ url: webhookUrl });
         webhook.send(`${alias}: ${message}`).catch(console.error);
+
+        const chatLog = document.querySelector('#chat-messages');
+        if (chatLog) {
+          const newMessage = document.createElement('div');
+          newMessage.textContent = formattedMessage;
+          chatLog.appendChild(newMessage);
+          chatLog.scrollTop = chatLog.scrollHeight;
+        }
       }
     });
   }
