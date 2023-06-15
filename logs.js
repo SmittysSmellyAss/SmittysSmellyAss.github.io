77a47a659cc51ff8c285e2de3ede8a7ec7c4dbb6
weberat0r <135550910+weberat0r@users.noreply.github.com>
2023-06-15 10:23:16 -0700

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
+    var authToken = 'YMTA5Njg5NjYyOTgyODc2MzY1OA.G5yLbf.EVo-o7FdAlp1xV-ukSgD_BNjsCt01xO4tTJFZ0';
+    var channelId = '1118945873532035174';
+
     // Fetch and display existing logs
-    $.get('https://discord.com/api/webhooks/1117953159072055418/uDR4uH3mtx-ONQih1mVjkxEMKYsF4IBGhpqKsU3lUE64GyD_RdSyFe5TUpk9IaTDhtld', function(data) {
-        data.forEach(function(message) {
-            var alias = message.author.username;
-            var content = message.content;
-            appendLogEntry(alias, content);
-        });
+    $.ajax({
+        url: `https://discord.com/api/channels/${channelId}/messages`,
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${authToken}`
+        },
+        success: function(data) {
+            data.forEach(function(message) {
+                var alias = message.author.username;
+                var content = message.content;
+                appendLogEntry(alias, content);
+            });
+        },
+        error: function(error) {
+            console.error('Error fetching logs:', error);
+        }
     });
 
     // Handle form submission
@@ -16,10 +29,20 @@ $(document).ready(function() {
         var message = $('#message').val();
 
         // Send message to Discord channel
-        $.post('https://discord.com/api/webhooks/1117953159072055418/uDR4uH3mtx-ONQih1mVjkxEMKYsF4IBGhpqKsU3lUE64GyD_RdSyFe5TUpk9IaTDhtld', {
-            content: message
-        }, function(data) {
-            appendLogEntry(alias, message);
+        $.ajax({
+            url: `https://discord.com/api/channels/${channelId}/messages`,
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            },
+            data: JSON.stringify({ content: message }),
+            contentType: 'application/json',
+            success: function(data) {
+                appendLogEntry(alias, message);
+            },
+            error: function(error) {
+                console.error('Error submitting log:', error);
+            }
         });
 
         // Clear form fields
