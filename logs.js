034dade4b2a3c3fcde1c0f0ac25f2c627a8f1486
weberat0r <135550910+weberat0r@users.noreply.github.com>
2023-06-15 09:52:30 -0700

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,19 +1,35 @@
-document.getElementById('submissionForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+$(document).ready(function() {
+    // Fetch and display existing logs
+    $.get('https://discord.com/api/channels/1118945873532035174/messages', function(data) {
+        data.forEach(function(message) {
+            var alias = message.author.username;
+            var content = message.content;
+            appendLogEntry(alias, content);
+        });
+    });
 
-    // Get form values
-    var alias = document.getElementById('alias').value;
-    var message = document.getElementById('message').value;
+    // Handle form submission
+    $('#submissionForm').submit(function(event) {
+        event.preventDefault();
 
-    // Create log entry
-    var logEntry = document.createElement('p');
-    logEntry.innerText = alias + ': ' + message;
+        var alias = $('#alias').val();
+        var message = $('#message').val();
 
-    // Add log entry to logbook
-    var logbook = document.getElementById('logbook');
-    logbook.appendChild(logEntry);
+        // Send message to Discord channel
+        $.post('https://discord.com/api/channels/1118945873532035174/messages', {
+            content: message
+        }, function(data) {
+            appendLogEntry(alias, message);
+        });
 
-    // Clear form fields
-    document.getElementById('alias').value = '';
-    document.getElementById('message').value = '';
+        // Clear form fields
+        $('#alias').val('');
+        $('#message').val('');
+    });
+
+    // Helper function to append log entry
+    function appendLogEntry(alias, message) {
+        var logEntry = '<p>' + alias + ': ' + message + '</p>';
+        $('#logbook').append(logEntry);
+    }
 });
