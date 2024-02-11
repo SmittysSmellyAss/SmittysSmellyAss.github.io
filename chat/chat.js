e5021fb9a875fd910f9579b10e29dedd4205a0c3
weberat0r <135550910+weberat0r@users.noreply.github.com>
2024-02-11 13:26:07 -0700

    Update chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -1,1 +1,8 @@
+const ws_url = 'wss://rune-thirsty-zipper.glitch.me:8080';
+const Socket = new WebSocket(ws_url)
+
+
+Socket.onopen = (event) => {
+  Socket.send("Someone just opened the chat");
+};
 
