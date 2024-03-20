72cfb2de0829ee370008e1ed30dc75f9a12cab42
SmittysSmellyAss <huggiemanguy@gmail.com>
2024-03-19 23:37:17 -0400

    Update stuff.js with new image

--- csgo/stuff.js
+++ csgo/stuff.js
@@ -8,7 +8,8 @@ const images = [
     "pix/7.png",
     "pix/8.png",
     "pix/9.png",
-    "pix/10.png"
+    "pix/10.png",
+    "pix/11.png"
 ];
 
 const carouselSlide = document.getElementById('carouselSlide');
