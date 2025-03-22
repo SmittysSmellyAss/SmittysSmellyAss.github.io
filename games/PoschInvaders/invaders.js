228c260329ca0c792cbec22f0d826e03e3de8099
weberat0r <135550910+weberat0r@users.noreply.github.com>
2025-03-21 23:20:53 -0700

    Update invaders.js

--- games/PoschInvaders/invaders.js
+++ games/PoschInvaders/invaders.js
@@ -113,7 +113,7 @@ function submitScore(playerName, score) {
         return;
     }
 
-    fetch("https://5d72f246-c007-466d-bd5e-7e6d302a223d-00-wvskqeapdfqt.riker.replit.dev/leaderboard", {
+    fetch("https://5d72f246-c007-466d-bd5e-7e6d302a223d-00-wvskqeapdfqt.riker.replit.dev:3000/leaderboard", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
